b3b340764c2f98e9e6393c9e259a7dd7b697167b
Arnaud Legendre <arnaudleg@gmail.com>
2014-07-16 17:05:46 -0700

    Add constant for ESX datacenter path (HTTP access)
    This patch adds a constant.py file to store the constants needed in
    the VMware ecosystem. A new constant is added for the ESX datacenter
    path when using http access to datastores.
    
    Change-Id: Ie5b84b3cc3913ab57f7ab487349557781cc4157a

---
+++ oslo/vmware/constants.py
@@ -0,0 +1,23 @@
+# Copyright (c) 2014 VMware, Inc.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+
+"""
+Shared constants across the VMware ecosystem.
+"""
+
+# Datacenter path for HTTP access to datastores if the target server is an ESX/
+# ESXi system: http://goo.gl/B5Htr8 for more information.
+ESX_DATACENTER_PATH = 'ha-datacenter'
