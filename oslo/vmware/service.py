1c2987bc4cb19fd53299abb8783763042348a768
Arnaud Legendre <arnaudleg@gmail.com>
2014-09-22 12:08:54 -0700

    getText can be called only when doc is not None
    Add a check to make sure getText is executed only when doc is
    not None.
    Without this check, we can potentially get:
    AttributeError: 'NoneType' object has no attribute 'getText'
    
    Change-Id: I8e2abd3abf015733642419cb8a3b86aec09c929f

--- oslo/vmware/service.py
+++ oslo/vmware/service.py
@@ -235,9 +235,10 @@ class Service(object):
 
             except suds.WebFault as excep:
                 doc = excep.document
-                fault_string = doc.childAtPath("/Envelope/Body/Fault/"
-                                               "faultstring").getText()
-                detail = doc.childAtPath('/Envelope/Body/Fault/detail')
+                if doc is not None:
+                    fault_string = doc.childAtPath("/Envelope/Body/Fault/"
+                                                   "faultstring").getText()
+                    detail = doc.childAtPath('/Envelope/Body/Fault/detail')
                 fault_list = []
                 details = {}
                 if detail:
