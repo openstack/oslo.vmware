7ebc48cf9dd5d1445e8ab59ad6fb3b1dc517c4c2
Doug Hellmann <doug@doughellmann.com>
2015-05-05 19:20:28 +0000

    Remove oslo namespace package
    Blueprint remove-namespace-packages
    
    Depends-on: Id86b74fb492e1b86d29197b4e127d8368affa3fd
    for openstack/congress
    
    Change-Id: I3db0b8da385cb4ca475129260725a66413b25854

--- oslo_vmware/tests/test_vim.py
+++ oslo_vmware/tests/test_vim.py
@@ -23,7 +23,7 @@ import mock
 from oslo_i18n import fixture as i18n_fixture
 import suds
 
-from oslo.vmware import exceptions
+from oslo_vmware import exceptions
 from oslo_vmware.tests import base
 from oslo_vmware import vim
 
