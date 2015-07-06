3ddcf2ac858905faf3c92ff6b3581c82006e0745
Vipin Balachandran <vbala@vmware.com>
2015-07-06 11:22:11 +0530

    Mask/truncate vCenter credentials in suds logs
    Suds logs will contain vCenter credentials if VIM Login,
    SessionIsActive APIs fail. This patch adds a logging
    filter to mask/truncate vCenter credentials in suds
    logs.
    
    SecImpact
    Suds will log vCenter username, password if vCenter
    Login API call fails.
    
    Change-Id: Idf7276528f72a9e590f2d0134619d8039c3f5b7d
    Co-Authored-By: Radoslav Gerganov<rgerganov@vmware.com>
    Closes-Bug: #1471656
    (cherry picked from commit d8ccf388f93f4256b3bd70c138236f1866ff98a5)

--- oslo_vmware/tests/test_service.py
+++ oslo_vmware/tests/test_service.py
@@ -476,3 +476,77 @@ class RequestsTransportTest(base.TestCase):
         with mock.patch(builtin_open, open_mock, create=True):
             resp = transport.session.send(request)
             self.assertEqual(data, resp.content)
+
+
+class SudsLogFilterTest(base.TestCase):
+    """Tests for SudsLogFilter."""
+
+    def setUp(self):
+        super(SudsLogFilterTest, self).setUp()
+        self.log_filter = service.SudsLogFilter()
+
+        self.login = mock.Mock(spec=suds.sax.element.Element)
+        self.username = suds.sax.element.Element('username').setText('admin')
+        self.password = suds.sax.element.Element('password').setText(
+            'password')
+        self.session_id = suds.sax.element.Element('session_id').setText(
+            'abcdef')
+
+        def login_child_at_path_mock(path):
+            if path == 'userName':
+                return self.username
+            if path == 'password':
+                return self.password
+            if path == 'sessionID':
+                return self.session_id
+
+        self.login.childAtPath.side_effect = login_child_at_path_mock
+
+    def test_filter_with_no_child_at_path(self):
+        message = mock.Mock(spec=object)
+        record = mock.Mock(msg=message)
+        self.assertTrue(self.log_filter.filter(record))
+
+    def test_filter_with_login_failure(self):
+        message = mock.Mock(spec=suds.sax.element.Element)
+
+        def child_at_path_mock(path):
+            if path == '/Envelope/Body/Login':
+                return self.login
+
+        message.childAtPath.side_effect = child_at_path_mock
+        record = mock.Mock(msg=message)
+
+        self.assertTrue(self.log_filter.filter(record))
+        self.assertEqual('***', self.username.getText())
+        self.assertEqual('***', self.password.getText())
+        self.assertEqual('bcdef', self.session_id.getText())
+
+    def test_filter_with_session_is_active_failure(self):
+        message = mock.Mock(spec=suds.sax.element.Element)
+
+        def child_at_path_mock(path):
+            if path == '/Envelope/Body/SessionIsActive':
+                return self.login
+
+        message.childAtPath.side_effect = child_at_path_mock
+        record = mock.Mock(msg=message)
+
+        self.assertTrue(self.log_filter.filter(record))
+        self.assertEqual('***', self.username.getText())
+        self.assertEqual('***', self.password.getText())
+        self.assertEqual('bcdef', self.session_id.getText())
+
+    def test_filter_with_unknown_failure(self):
+        message = mock.Mock(spec=suds.sax.element.Element)
+
+        def child_at_path_mock(path):
+            return None
+
+        message.childAtPath.side_effect = child_at_path_mock
+        record = mock.Mock(msg=message)
+
+        self.assertTrue(self.log_filter.filter(record))
+        self.assertEqual('admin', self.username.getText())
+        self.assertEqual('password', self.password.getText())
+        self.assertEqual('abcdef', self.session_id.getText())
