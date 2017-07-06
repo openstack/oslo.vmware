01443b83803ff6348033ccaf5cc737047c02162b
Akihiro Motoki <amotoki@gmail.com>
2017-07-06 21:09:13 +0000

    Switch from oslosphinx to openstackdocstheme
    Change-Id: Ic1e2786783fa47f305f8a849d36648efe3e46ac4

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -24,8 +24,13 @@ sys.path.insert(0, os.path.abspath('../..'))
 extensions = [
     'sphinx.ext.autodoc',
     #'sphinx.ext.intersphinx',
-    'oslosphinx'
+    'openstackdocstheme'
 ]
+
+# openstackdocstheme options
+repository_name = 'openstack/oslo.vmware'
+bug_project = 'oslo.vmware'
+bug_tag = ''
 
 # autodoc generation is a bit aggressive and a nuisance when doing heavy
 # text edit cycles.
@@ -76,6 +81,9 @@ pygments_style = 'sphinx'
 # html_theme_path = ["."]
 # html_theme = '_theme'
 # html_static_path = ['static']
+html_theme = 'openstackdocs'
+
+html_last_updated_fmt = '%Y-%m-%d %H:%M'
 
 # Output file base name for HTML help builder.
 htmlhelp_basename = '%sdoc' % project
