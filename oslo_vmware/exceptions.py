7370c400c4f87b6a74a151c69108216140d71fb6
Hemna <waboring@hemna.com>
2020-02-05 17:58:00 -0500

    Ensure lease polling raises proper exception
    This patch adds the ability to properly translate and raise
    VimExceptions when polling the state of the lease.
    
    Change-Id: Ie22808471d4c72e26607e817167e0e2283630b5a

--- oslo_vmware/exceptions.py
+++ oslo_vmware/exceptions.py
@@ -296,6 +296,32 @@ def get_fault_class(name):
     return fault_class
 
 
+def translate_fault(localized_method_fault, excep_msg=None):
+    """Produce proper VimException subclass object,
+
+    The exception is based on a vmodl.LocalizedMethodFault.
+
+    :param excep_msg: Message to set to the exception. Defaults to
+                      localizedMessage of the fault.
+    """
+    try:
+        if not excep_msg:
+            excep_msg = six.text_type(localized_method_fault.localizedMessage)
+        name = localized_method_fault.fault.__class__.__name__
+        fault_class = get_fault_class(name)
+        if fault_class:
+            ex = fault_class(excep_msg)
+        else:
+            ex = VimFaultException([name], excep_msg)
+    except Exception as e:
+        LOG.debug("Unexpected exception thrown (%s) while translating"
+                  " fault (%s) with message: %s.",
+                  e, localized_method_fault, excep_msg)
+        ex = VimException(message=excep_msg, cause=e)
+
+    return ex
+
+
 def register_fault_class(name, exception):
     fault_class = _fault_classes_registry.get(name)
     if not issubclass(exception, VimException):
